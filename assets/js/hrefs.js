508ccb91354767f16679dcf1ac8a74536bb9eccf
banasaron <aron.banas@wp.pl>
2024-01-11 21:41:07 +0100

    fixing contact form

--- assets/js/hrefs.js
+++ assets/js/hrefs.js
@@ -1,18 +1,14 @@
-const fb = $("#fb");
 const gh = $("#gh");
 const english = $("#toEnglish");
 const polish = $("#toPolish");
 
-fb.click(()=>{
-    window.open("https://www.facebook.com/profile.php?id=100080106387894&locale=pl_PL", "_blank");
-});
 gh.click(()=>{
     window.open("https://github.com/banasaron", "_blank");
 });
 
 english.click(()=>{
-    window.open("../../index.html");
+    window.open("../index.html", "_self");
 });
 polish.click(()=>{
-    window.open("assets/lang/page-polish.html")
+    window.open("lang/page-pl.html", "_self")
 });
